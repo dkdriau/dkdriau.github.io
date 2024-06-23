073c7da2102dbaa9da7d44bac897c068fefa38b1
rayhans21 <rayhan2102@gmail.com>
2024-06-23 19:17:59 +0700

    penambahan linktree

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Linktree />
+  </div>
+</template>
+
+<script>
+import Linktree from './components/dkdlink.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Linktree,
+  },
+};
+</script>
+
+<style>
+@import 'tailwindcss/tailwind.css';
+</style>
